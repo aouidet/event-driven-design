3fade9684e2febfbf3f71688d2d386426d3cd30b
aouidet oussama <aouidetoussama@mbp-de-aouidet.home>
2025-08-01 23:14:05 +0200

    fieat / implemented jasypt encryption

---
+++ playground/src/main/java/jasypt/TestJasypt.java
@@ -0,0 +1,18 @@
+package jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+
+public class TestJasypt {
+
+    public static void main(String[] args) {
+        StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+        standardPBEStringEncryptor.setPassword("Demo_pwd!2025");
+        standardPBEStringEncryptor.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        standardPBEStringEncryptor.setIvGenerator(new RandomIvGenerator());
+
+        String result = standardPBEStringEncryptor.encrypt("springCloud-Pwd!");
+        System.out.println(result);
+        System.out.println(standardPBEStringEncryptor.decrypt(result));
+    }
+}
